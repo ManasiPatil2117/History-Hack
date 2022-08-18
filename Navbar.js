cf4b38d1d61b253109ca59169c5459a86e78c544
Manasi <manasipatilap2117@gmail.com>
2022-08-18 14:21:14 +0530

    India

---
+++ Navbar.js
@@ -0,0 +1,58 @@
+document.write(`<nav class="navbar navbar-expand-lg bg-light p-3">
+<div class="container-fluid">
+  <a class="navbar-brand fw-bold" href="#">History Hack</a>
+  <button
+    class="navbar-toggler"
+    type="button"
+    data-bs-toggle="collapse"
+    data-bs-target="#navbarSupportedContent"
+    aria-controls="navbarSupportedContent"
+    aria-expanded="false"
+    aria-label="Toggle navigation"
+  >
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="Index.html">Home</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a
+          class="nav-link dropdown-toggle"
+          href="#"
+          role="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Countries
+        </a>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="India.html">India</a></li>
+          <li><a class="dropdown-item" href="Austria.html">Austria</a></li>
+          <li><a class="dropdown-item" href="Egypt.html">Egypt</a></li>
+          <li><a class="dropdown-item" href="Czech_Republic.html">Czech Republic</a></li>
+          <li><a class="dropdown-item" href="France.html">France</a></li>
+          <li><a class="dropdown-item" href="Germany.html">Germany</a></li>
+          <li><a class="dropdown-item" href="Italy.html">Italy</a></li>
+          <li><a class="dropdown-item" href="Peru.html">Peru</a></li>
+          <li><a class="dropdown-item" href="Spain.html">Spain</a></li>
+          <li><a class="dropdown-item" href="Turkey.html">Turkey</a></li>
+          
+        </ul>
+      </li>
+    </ul>
+    <form class="d-flex" role="search">
+      <input
+        class="form-control me-2"
+        type="search"
+        placeholder="Search"
+        aria-label="Search"
+      />
+      <button class="btn btn-outline-success" type="submit">
+        Search
+      </button>
+    </form>
+  </div>
+</div>
+</nav>`);

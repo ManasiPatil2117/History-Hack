4653be75d7889cd9fabd278390f842d4ecc62bfe
Manasi <manasipatilap2117@gmail.com>
2022-08-18 14:59:02 +0530

    Peru

--- Navbar.js
+++ Navbar.js
@@ -1,6 +1,6 @@
 document.write(`<nav class="navbar navbar-expand-lg bg-light p-3">
 <div class="container-fluid">
-  <a class="navbar-brand fw-bold" href="#">History Hack</a>
+  <a class="navbar-brand fw-bold" href="Index.html">History Hack</a>
   <button
     class="navbar-toggler"
     type="button"
